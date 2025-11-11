b503ec83861de632cfc386fe5e21d15771167c94
Ahtat204 <lahcen28ahtat@gmail.com>
2025-11-11 23:13:07 +0100

    working on jwt authentication and authorization

--- TeamcollborationHub.server/Program.cs
+++ TeamcollborationHub.server/Program.cs
@@ -1,29 +1,61 @@
 using Microsoft.EntityFrameworkCore;
 using TeamcollborationHub.server.Configuration;
 using TeamcollborationHub.server.Services.Authentication;
+using Microsoft.AspNetCore.Authentication.BearerToken;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using TeamcollborationHub.server.Services.Caching;
+using Microsoft.IdentityModel.Tokens;
+using dotenv.net;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+DotEnv.Load();
+// Add services to the container.
+builder.Configuration
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddEnvironmentVariables();
 builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<TDBContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("SQLServerConnectionString") ?? throw new InvalidOperationException("Connection string 'TeamcollborationHubContext' not found.")));
 builder.Services.AddStackExchangeRedisCache(options =>
 {
-    options.Configuration = builder.Configuration.GetConnectionString("RedisConnectionString");
-    options.InstanceName = builder.Configuration.GetValue<string>("RedisInstanceName") ?? "DefaultInstance";
+    options.Configuration=builder.Configuration.GetConnectionString("RedisConnectionString");
+    options.InstanceName=builder.Configuration.GetValue<string>("RedisInstanceName")??"DefaultInstance";
 });
-builder.Services.AddScoped<ICachingService, RedisCachingService>();
+builder.Services.AddScoped<ICachingService,RedisCachingService>();
 builder.Services.AddKeyedScoped<IAuthenticationService, AuthenticationService>("AuthenticationService");
 builder.Services.AddSingleton<TDBContext>();
+builder.Services.AddAuthentication(opt => {
+    opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["JwtConfig:Issuer"],
+            ValidAudience = builder.Configuration["JwtConfig:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JwtConfi:Key"]!))
+        };
+    });
+builder.Services.AddAuthorization();
 var app = builder.Build();
+
+// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 app.UseHttpsRedirection();
+app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
--- TeamcollborationHub.server/Services/Authentication/AuthenticationService.cs
+++ TeamcollborationHub.server/Services/Authentication/AuthenticationService.cs
@@ -1,5 +1,32 @@
-﻿namespace TeamcollborationHub.server.Services.Authentication;
+﻿using TeamcollborationHub.server.Entities;
+using TeamcollborationHub.server.Repositories;
+
+namespace TeamcollborationHub.server.Services.Authentication;
 
 public class AuthenticationService : IAuthenticationService
 {
+    private readonly AuthenticationRepository _authenticationRepository;
+
+    public AuthenticationService(AuthenticationRepository authenticationRepository)=>_authenticationRepository = authenticationRepository;
+    
+
+    public User? createUser()
+    {
+        throw new NotImplementedException();
+    }
+
+    public User? deleteUser(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public User? GetUserbyId()
+    {
+        throw new NotImplementedException();
+    }
+
+    public User? updateUser(User user)
+    {
+        throw new NotImplementedException();
+    }
 }

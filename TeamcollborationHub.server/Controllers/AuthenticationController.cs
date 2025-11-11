5e4228249d4a6e5e9a69710a695f0904966de6f9
Ahtat204 <lahcen28ahtat@gmail.com>
2025-11-11 23:13:07 +0100

    working on jwt authentication and authorization

--- TeamcollborationHub.server/Controllers/AuthenticationController.cs
+++ TeamcollborationHub.server/Controllers/AuthenticationController.cs
@@ -1,11 +1,24 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿namespace TeamcollborationHub.server.Controllers;
 
-namespace TeamcollborationHub.server.Controllers
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TeamcollborationHub.server.Entities;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthenticationController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class AuthenticationController : ControllerBase
+    [HttpGet("/login")]
+    public async Task<ActionResult<User>> Login([FromBody] User user)
     {
+        if(user is null) { return BadRequest("Invalid user data"); }
+        return Ok();///TODO: implement login logic
+    }
+
+
+    [HttpPost("/signup")]
+    public async Task<ActionResult<User>> SignUp([FromBody] User user)
+    {
+        return Ok();///TODO: implement signup logic
     }
 }
--- TeamcollborationHub.server/Services/Authentication/IAuthenticationService.cs
+++ TeamcollborationHub.server/Services/Authentication/IAuthenticationService.cs
@@ -1,6 +1,12 @@
-﻿namespace TeamcollborationHub.server.Services.Authentication;
+﻿using TeamcollborationHub.server.Entities;
+
+namespace TeamcollborationHub.server.Services.Authentication;
 
     public interface IAuthenticationService
     {
-    }
+    public User? GetUserbyId();
+    public User? createUser();
+    public User? updateUser(User user);
+    public User? deleteUser(int id);
+}
 

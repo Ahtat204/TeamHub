64d6bddde25f3b71e11bbd05a912be53c7bde4a9
Ahtat204 <lahcen28ahtat@gmail.com>
2026-01-11 15:46:11 +0100

    added routing

--- teamcollaborationhub.client/src/app/app-routing-module.ts
+++ teamcollaborationhub.client/src/app/app-routing-module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from './user-login/user-login';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', redirectTo:"user-login",pathMatch:"full" },{path:"user-login",component:LoginComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

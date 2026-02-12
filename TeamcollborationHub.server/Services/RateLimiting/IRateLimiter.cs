6632e8844ad2b5b3041281501defae93e0df5731
Lahcen <lahcen28ahtat@gmail.com>
2026-02-12 19:29:13 +0100

    added missing services

---
+++ TeamcollborationHub.server/Services/RateLimiting/IRateLimiter.cs
@@ -0,0 +1,7 @@
+﻿namespace TeamcollborationHub.server.Services.RateLimiting
+{
+    public interface IRateLimiter
+    {
+        public Task InvokeAsync(HttpContext context, RequestDelegate next);
+    }
+}

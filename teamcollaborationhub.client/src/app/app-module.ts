5708228a706b1869b4f8d368484d52d3e1edcc0e
Lahcen <lahcen28ahtat@gmail.com>
2026-02-12 20:23:53 +0100

    adding header

--- teamcollaborationhub.client/src/app/app-module.ts
+++ teamcollaborationhub.client/src/app/app-module.ts
@@ -1,6 +1,5 @@
 import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { UserSignup } from './user-signup/user-signup';
@@ -10,6 +9,7 @@ import {MatCard, MatCardContent, MatCardHeader} from '@angular/material/card';
 import {MatFormField, MatSuffix} from '@angular/material/input';
 import {MatIcon} from '@angular/material/icon';
 import {MatInput} from '@angular/material/input';
+import { Dashboard } from './dashboard/dashboard';
 
 @NgModule({
   declarations: [
@@ -27,7 +27,8 @@ import {MatInput} from '@angular/material/input';
     MatFormField,
     MatIcon,
     MatInput,
-    MatSuffix
+    MatSuffix,
+    Dashboard
   ],
   providers: [
     provideBrowserGlobalErrorListeners()
--- teamcollaborationhub.client/src/app/app-routing-module.ts
+++ teamcollaborationhub.client/src/app/app-routing-module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {LoginComponent} from './user-login/user-login';
 import {UserSignup} from './user-signup/user-signup';
+import {Dashboard} from './dashboard/dashboard';
 
-const routes: Routes = [{ path: '', redirectTo:"",pathMatch:"full" },{path:"login",component:LoginComponent},{path:'register',component:UserSignup}];
+const routes: Routes = [{ path: '', redirectTo:"",pathMatch:"full" },{path:"login",component:LoginComponent},{path:'register',component:UserSignup},{path:"home",component:Dashboard}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

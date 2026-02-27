13c3752bb3b6007b2e018e828976be8e6e7407e6
Lahcen <lahcen28ahtat@gmail.com>
2026-02-27 13:47:41 +0100

    added back TdbContext.cs file that caused merge conflicts that looked like a circular dependency issue

---
+++ TeamcollborationHub.server/Configuration/TdbContext.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
+using TeamcollborationHub.server.Entities;
+
+namespace TeamcollborationHub.server.Configuration;
+
+public sealed class TdbContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+    public DbSet<Project> Projects { get; set; }
+    public DbSet<ProjectTask> Tasks { get; set; }
+    public DbSet<Comment> Comments { get; set; }
+    public DbSet<RefreshToken> RefreshTokens { get; set; }
+
+    public TdbContext(DbContextOptions<TdbContext> options) : base(options)
+    {
+        try
+        {
+            if (Database.GetService<IDatabaseCreator>() is RelationalDatabaseCreator databasecreator)
+            {
+                if(!databasecreator.Exists() || !databasecreator.CanConnect()) databasecreator.Create();
+                if(!databasecreator.HasTables()) databasecreator.CreateTables(); 
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.Error.WriteLine(ex.Message);
+        }
+    }
+
+    public TdbContext()
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Project>().HasMany(p => p.contributor).WithOne(u => u.project)
+            .HasForeignKey(u => u.ProjectId);
+        modelBuilder.Entity<Project>().HasMany(p => p.Tasks).WithOne(t => t.project).HasForeignKey(t => t.projectId);
+        modelBuilder.Entity<Project>().HasMany(p => p.comments).WithOne(c => c.Project).HasForeignKey(c => c.projectId);
+        modelBuilder.Entity<Project>().Property(o => o.status).HasConversion<string>();
+        modelBuilder.Entity<User>().HasIndex(u => u.Email).IsUnique();
+    }
+}
